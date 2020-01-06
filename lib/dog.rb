d548acb9bf743216c42e19f216518ffa69fde460
Alexis Priore <alexispriore@gmail.com>
2020-01-06 20:49:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,3 +14,23 @@ fido.name = "Fido"
 
 puts fido.name
 end
+
+
+class Dog
+		  def initialize(breed)
+		    @breed = breed
+		  end
+		 
+		  def breed=(breed)
+		    @breed = breed
+		  end
+		 
+		  def breed
+		    @breed
+		  end
+		end
+
+
+		fido = Dog.new(“Pug”)
+		 
+		fido.breed
